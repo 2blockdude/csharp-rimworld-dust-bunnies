208367ffc18629664cb2a49aea9cde528216d4f2
Anthony Cobb <2blockdude@gmail.com>
2021-11-12 00:31:30 -0500

    dust and bunnies

---
+++ Source/dust-bunnies-1.3/Recipe_SpawnDustBunny.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace DustBunnies
+{
+    public class Recipe_SpawnDustBunny : RecipeWorker
+    {
+        public static PawnKindDef Pawn_DustBunny = DefDatabase<PawnKindDef>.GetNamed("DustBunny");
+
+        public override void Notify_IterationCompleted(Pawn billDoer, List<Thing> ingredients)
+        {
+            Pawn dustBunny = PawnGenerator.GeneratePawn(Pawn_DustBunny, billDoer.Faction);
+            GenSpawn.Spawn(dustBunny, billDoer.Position, billDoer.Map, Rot4.Random);
+        }
+    }
+}
